e8155a73dc83f585772a486378b468b6c02a8037
Brant Smith <brant.lee.smith88@gmail.com>
2020-11-16 13:53:49 -0500

    fixed some errors and cleaned up the code

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const STORE = [
-  { id: cuid(), name: "apples", checked: false },
-  { id: cuid(), name: "oranges", checked: false },
-  { id: cuid(), name: "milk", checked: true },
-  { id: cuid(), name: "bread", checked: false },
-];
-
-function generateItemElement(item, index, temp) {
-  return `
-    <li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${
-        item.checked ? "shopping-item__checked" : ""
-      }">${item.name}</span>
-      <div class="shopping-item-controls">
-        <button class="shopping-item-toggle js-item-toggle">
-            <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete js-item-delete">
-            <span class="button-label">delete</span>
-        </button>
-      </div>
-    </li>`;
-}
-
-function generateShoppingItemsString(shoppingList) {
-  const items = shoppingList.map((item, index) =>
-    generateItemElement(item, index)
-  );
-
-  return items.join("");
-}
-
-function renderShoppingList() {
-  const shoppingListItemsString = generateShoppingItemsString(STORE);
-  $(".shopping-list").html(shoppingListItemsString);
-}
-
-function addNewItem(itemName) {
-  STORE.push({ id: cuid(), name: itemName, checked: false });
+function addNewItem(newItem) {
+  $(".shopping-list").append(`<li>
+  <span class="shopping-item">${newItem}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`);
 }
 
 function handleNewItem() {
@@ -45,42 +18,26 @@ function handleNewItem() {
     const newItem = $("#shopping-list-entry").val();
     $("#shopping-list-entry").val("");
     addNewItem(newItem);
-    renderShoppingList();
   });
-}
-
-function getItemIdFromElement(item) {
-  return $(item).closest("li").data("item-id");
-}
-
-function toggleChecked(id) {
-  const item = STORE.find((item) => item.id === id);
-  item.checked = !item.checked;
 }
 
 function handleItemCheckClicked() {
-  $(".shopping-list").on("click", ".js-item-toggle", (event) => {
-    const id = getItemIdFromElement(event.currentTarget);
-    toggleChecked(id);
-    renderShoppingList();
+  $(".shopping-list").on("click", ".shopping-item-toggle", (e) => {
+    $(e.currentTarget)
+      .closest("li")
+      .children()
+      .filter("span")
+      .toggleClass("shopping-item__checked");
   });
 }
 
-function deleteItem(itemId) {
-  const itemInd = STORE.findIndex((item) => item.id === itemId);
-  STORE.splice(itemInd, 1);
-}
-
 function handleItemDeleted() {
-  $(".shopping-list").on("click", ".js-item-delete", (e) => {
-    const itemId = getItemIdFromElement(e.currentTarget);
-    deleteItem(itemId);
-    renderShoppingList();
+  $(".shopping-list").on("click", ".shopping-item-delete", (e) => {
+    $(e.currentTarget).closest("li").remove();
   });
 }
 
 function handler() {
-  renderShoppingList();
   handleNewItem();
   handleItemCheckClicked();
   handleItemDeleted();
